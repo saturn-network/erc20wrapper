c848b0520433b853097b6bf33dbd69e2edf9f36f
Neuron <neuron@saturn.network>
2020-04-07 19:26:41 +0100

    Updates

---
+++ precache-manifest.037048e8bdef9f785d9e05510ba517ce.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42399f2c817614d1b4df5c20bf79ae35",
+    "url": "/erc20wrapper/index.html"
+  },
+  {
+    "revision": "5f93fdf91d13c1969e94",
+    "url": "/erc20wrapper/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5f93fdf91d13c1969e94",
+    "url": "/erc20wrapper/static/js/2.df710aea.chunk.js"
+  },
+  {
+    "revision": "e6b414fe7ba23fa63bb7093684d497a4",
+    "url": "/erc20wrapper/static/js/2.df710aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce1e77fcf024be1384d",
+    "url": "/erc20wrapper/static/js/main.01d80caa.chunk.js"
+  },
+  {
+    "revision": "81d682fd52c02f68d576",
+    "url": "/erc20wrapper/static/js/runtime-main.62be18bb.js"
+  },
+  {
+    "revision": "1769ac4302ca27043febe1123101331c",
+    "url": "/erc20wrapper/static/media/install-wallet.1769ac43.png"
+  },
+  {
+    "revision": "2fe9aec21b7b9c4f7efacc442d9e438f",
+    "url": "/erc20wrapper/static/media/logo-light.2fe9aec2.png"
+  },
+  {
+    "revision": "f987d386d04658cd0b9bd98e1552792f",
+    "url": "/erc20wrapper/static/media/saturn-20.f987d386.png"
+  }
+]);
